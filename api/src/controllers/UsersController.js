d2f6a7ce8b3dd014e2bdc5fe513172c05f342721
Andrelicio Santos <andreliciosantos@gmail.com>
2023-08-17 23:34:23 -0300

    adcionando criptografia com a biblioteca bcryptjs

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -8,6 +8,8 @@
    * delete - DELETE para remover um registro.
    */
 
+const { hash } = require("bcryptjs");
+
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
@@ -22,7 +24,9 @@ class UsersController {
     throw new AppError("Este e-mail já está em uso.");
    }
 
-   await database.run("INSERT INTO users (name, email, password)  VALUES (?, ?, ?)", [name,email,password]);
+   const hashedPassword = await hash(password, 8);
+
+   await database.run("INSERT INTO users (name, email, password)  VALUES (?, ?, ?)", [name,email,hashedPassword]);
 
    return response.status(201).json();
   }
