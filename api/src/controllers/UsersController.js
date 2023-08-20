84b0c8435b6f6dadf962cf30b7f713ecc9d8b49f
Andrelicio Santos <andreliciosantos@gmail.com>
2023-08-20 20:48:03 -0300

    fix update user name and email

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -48,8 +48,8 @@ class UsersController {
       throw new AppError("Este email ja esta em uso.");
     }
 
-    user.name = name;
-    user.email = email;
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
 
     if(password && !old_password){
       throw new AppError ("voce precisa informar a senha antiga");
